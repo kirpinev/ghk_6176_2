36575b67d77f695ab951f0575bc63e92af5c776d
Кирпинев Игорь Валерьевич <IKirpinev@alfabank.ru>
2025-08-26 11:33:27 +0300

    init

---
+++ src/thx/ThxLayout.tsx
@@ -0,0 +1,52 @@
+import { Typography } from "@alfalab/core-components/typography";
+import { ButtonMobile } from "@alfalab/core-components/button/mobile";
+import bigSmile from "../assets/bigSmile.png";
+import { thxSt } from "./style.css";
+import { appSt } from "../style.css.ts";
+import { Gap } from "@alfalab/core-components/gap";
+
+export const ThxLayout = () => {
+    const submit = () => {
+        window.gtag("event", "6176_get_real_sub", {
+            variant_name: "6176_2",
+        });
+    };
+
+    return (
+        <>
+            <div className={thxSt.container}>
+                <img
+                    alt="Картинка ракеты"
+                    src={bigSmile}
+                    width={250}
+                    className={thxSt.rocket}
+                />
+                <Typography.TitleResponsive
+                    font="system"
+                    tag="h1"
+                    view="large"
+                    defaultMargins={false}
+                    weight="bold"
+                >
+                    Такого вы точно не ожидали
+                </Typography.TitleResponsive>
+                <Gap size={8} />
+                <Typography.Text tag="p" view="primary-medium">
+                    Вы только что поучаствовали в настоящем эксперименте! Сейчас мы
+                    тестируем сервис, скоро предложим лучшие условия.
+                </Typography.Text>
+            </div>
+
+            <div className={appSt.bottomBtn}>
+                <ButtonMobile
+                    block
+                    view="primary"
+                    href="https://online.alfabank.ru/"
+                    onClick={submit}
+                >
+                    Спасибо, понятно!
+                </ButtonMobile>
+            </div>
+        </>
+    );
+};
